8e091d93d6455e497a2234aa5d7d7037b91bb9ad
garyteywx <igarytey36@gmail.com>
2023-02-21 22:56:17 +0800

    implemented login feature finally

--- TLM2008_Project/routes.py
+++ TLM2008_Project/routes.py
@@ -1,25 +1,35 @@
-from flask import Flask, render_template
-from flaskr import db
+from flask import Flask, render_template, Blueprint
+from flask_sqlalchemy import SQLAlchemy
+from . import db
 #from . import auth
 
-app = Flask(__name__)
+#main = Flask(__name__)
+main = Blueprint('main',__name__)
+#main.config['SQLALCHEMY_DATABASE_URI']='sqlite:///poptrade.db'
+#db = SQLAlchemy(main)
 
-#what does the @app.route() do? well, the @ symbol is a function extender for the function "app". So when app.route() is passed with "/", the function index() is called as an extension
 
-@app.route("/")
+# class user(db.Model):
+#     uid = db.Column(db.Integer,primary_key=True)
+#     content = db.Column()
+#what does the @main.route() do? well, the @ symbol is a function extender for the function "main". So when main.route() is passed with "/", the function index() is called as an extension
+
+@main.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/auction")
+@main.route("/auction")
 def auction():
     return render_template("/auction/auctionMain.html")
 
-@app.route("/register")
+@main.route("/register")
 def register():
     return render_template("/auth/register.html")
 
+#db.init_app(main)
 
-#app.register_blueprint(auth.bp)
+
+#main.register_blueprint(auth.bp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    main.run(debug=True)
